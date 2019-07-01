cb7a2cb78316078d131d5a3624f05035e5b6abf3
Clinton Lunn <cpl09c@gmail.com>
2019-07-01 19:55:15 -0400

    add sorting

--- index.js
+++ index.js
@@ -14,17 +14,37 @@ try {
     console.error(err)
 }
 
+// sort names by last, then first
+var compareNames = function (a, b) {
+    if (a.Last_Name < b.Last_Name) {
+        return -1;
+    }
+    if (a.Last_Name > b.Last_Name) {
+        return 1;
+    }
+    if (a.First_Name < b.First_Name) {
+        return -1;
+    }
+    if (a.First_Name > b.First_Name) {
+        return 1;
+    }
+    return;
+}
 
 async function getCSV() {
     return csvtojson().fromFile("./data.csv")
         .then(async source => {
-            let uniqueInsurance = await [...new Set(source.map(item => item.Insurance_Company))];
+            const uniqueInsurance = await [...new Set(source.map(item => item.Insurance_Company))];
 
             for (i in uniqueInsurance) {
                 const result = source.filter(element => element.Insurance_Company === uniqueInsurance[i]);
+                const sortedResult = result.sort(compareNames);
+                console.log({
+                    sortedResult
+                });
 
                 const keys = Object.keys(source[0]);
-                const csv = jsontocsv(result, {
+                const csv = jsontocsv(sortedResult, {
                     fields: keys
                 });
 
