7d54aa833cf57dae319cc50ca8776e87c11ac982
Clinton Lunn <cpl09c@gmail.com>
2019-07-02 21:51:40 -0400

    working program

--- index.js
+++ index.js
@@ -15,15 +15,28 @@ try {
 }
 
 // sort names by last, then first
-const compareNames = function (a, b) {
+const compareFirstNames = function (a, b) {
     // split into first name and last name variables
     const splitStringA = a.First_Name_Last_Name.split(" ");
     const splitStringB = b.First_Name_Last_Name.split(" ");
 
     a.First_Name = splitStringA[0];
-    a.Last_Name = splitStringA[splitStringA.length - 1];
-
     b.First_Name = splitStringB[0];
+
+    if (a.First_Name < b.First_Name) {
+        return -1;
+    }
+    if (a.First_Name > b.First_Name) {
+        return 1;
+    }
+    return 0;
+}
+const compareLastNames = function (a, b) {
+    // split into first name and last name variables
+    const splitStringA = a.First_Name_Last_Name.split(" ");
+    const splitStringB = b.First_Name_Last_Name.split(" ");
+
+    a.Last_Name = splitStringA[splitStringA.length - 1];
     b.Last_Name = splitStringB[splitStringB.length - 1];
 
     if (a.Last_Name < b.Last_Name) {
@@ -32,61 +45,52 @@ const compareNames = function (a, b) {
     if (a.Last_Name > b.Last_Name) {
         return 1;
     }
-    if (a.First_Name < b.First_Name) {
+    return 0;
+}
+
+// sort names by last, then first
+const compareUserid = function (a, b, key) {
+    // split into first name and last name variables
+
+    if (a.Userid < b.Userid) {
         return -1;
     }
-    if (a.First_Name > b.First_Name) {
+    if (a.Userid > b.Userid) {
         return 1;
     }
     return;
 }
+const removeDups = (companies) => {
+    const noDups = companies.filter((curr, index) => {
+        // base case
 
-// sort by increasing insurance versions
-const compareInsVersions = function (a, b) {
-    return b - a
-};
-
-function find_duplicate_in_array(arr) {
-    var object = {};
-    var result = [];
-
-    arr.forEach(function (item) {
-        if (!object[item])
-            object[item] = 0;
-        object[item] += 1;
-    })
-
-    for (var prop in object) {
-        if (object[prop] >= 2) {
-            result.push(prop);
+        if (index == 0) {
+            return true;
+        } else {
+            return (companies[index].Userid === companies[index - 1].Userid) ? false : true;
         }
-    }
 
-    return result;
 
+    });
+    return noDups;
 }
+
 async function getCSV() {
     return csvtojson().fromFile("./data.csv")
         .then(async source => {
             const uniqueInsurance = await [...new Set(source.map(item => item.Insurance_Company))];
 
             for (i in uniqueInsurance) {
-                const result = source.filter(element => element.Insurance_Company === uniqueInsurance[i]);
-                const sortedResult = result.sort(compareNames);
-                // TODO: sort results, remove First_Name, Last_Name properties (not included in original question)
-
-                let userIdHolder = [];
-                for (i in sortedResult) {
-                    userIdHolder.push(sortedResult[i].Userid);
-                }
-                const duplicates = find_duplicate_in_array(userIdHolder);
-                console.log(duplicates);
-
-
-                // console.log(userIdHolder);
-
-
-
+                const result = source.filter(element => {
+                    return element.Insurance_Company === uniqueInsurance[i]
+                });
+                // transform data
+                const sortedUserid = result.sort(compareUserid);
+                const uniqueArray = removeDups(sortedUserid)
+                const sortedFirstName = uniqueArray.sort(compareFirstNames);
+                const sortedLastName = sortedFirstName.sort(compareLastNames);
+                const sortedResult = sortedLastName
+                // write data back to csv
                 const keys = Object.keys(source[0]);
                 const csv = jsontocsv(sortedResult, {
                     fields: keys
